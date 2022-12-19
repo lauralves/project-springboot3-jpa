12972e2cbb6a4ab6e9341a3ccc3937f2ab992de7
lauralves <105663730+lauralves@users.noreply.github.com>
2022-12-19 13:46:31 -0300

    User entity and resource

---
+++ src/main/java/com/projetospring/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.projetospring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetospring.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maira", "maira@gmail.com", "999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
